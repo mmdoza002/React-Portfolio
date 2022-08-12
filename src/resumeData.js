a69214de48d47c95ade40d6825a8b24750f7993c
mmdoza002 <mmdoza002@gmail.com>
2022-08-12 15:41:33 -0500

    Beautiful Code

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Martin Mendoza",
+    "role": "Junior Full Stack Developer",
+    "linkedinId": " https://www.linkedin.com/in/martin-mendoza-614704167",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I am starting off my journey in the world of web development. On my freetime, I like to learn new programs to expand my horizon.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/martin-mendoza-614704167/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mmdoza002",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a student at UT Coding Bootcamp. I am in the process of perfecting Full Stack Web Development, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Austin,Tx",
+    "website":"Mmdoza002@gmail.com",
+    "education":[
+      {
+        "UniversityName":" UT Comding Bootcamp",
+        "specialization":"Full Stack Development",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2022",
+        "Achievements":"HTML, CSS, JAVASCRIPT, EXPRESS.JS, NODE.JS, MVC, ORM, REACT, OOP, MYSQL, WEB API's"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Capitol Printing",
+        "specialization":"Pressman",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Learning And Understanding A Heidelberg Printing Press"
+      },
+      {
+        "CompanyName":"Terex Auto Body",
+        "specialization":"Automotive Collision Repair And Restoration",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2013",
+        "Achievements":"Auto Paint And Paint Preperation / Minor Body Work."
+      },
+      {
+        "CompanyName":"Delstar Technology",
+        "specialization":"Production Crew",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2015",
+        "Achievements":"Measurments And Detailed Specs For Imported Plastic Material"
+      },
+      {
+        "CompanyName":"Lossen Bros",
+        "specialization":"Construction Clean Up",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2014",
+        "Achievements":"Understanding Water Run Off While Keeping A Clean Environment."
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"React.js"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"HTML"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"https://media.discordapp.net/attachments/1004472530012229662/1007000091153928244/Screen_Shot_2022-08-10_at_1.54.17_PM.png?width=1298&height=676"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"https://cdn.discordapp.com/attachments/1004472530012229662/1007000090436714546/Screen_Shot_2022-08-10_at_1.55.29_PM.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"https://scontent.xx.fbcdn.net/v/t1.15752-9/297779231_749641766115809_8721575030337421360_n.jpg?stp=dst-jpg_p206x206&_nc_cat=105&ccb=1-7&_nc_sid=aee45a&_nc_ohc=-Eu7YFKyvK8AX9CyE1k&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJ41z2JmcIw8UJ6kv5tnWvF5jDg0c88Sgokfw4UsQOVfA&oe=631BD2D1"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"https://scontent.xx.fbcdn.net/v/t1.15752-9/297958815_614464610282289_8101103446874382723_n.jpg?stp=dst-jpg_p206x206&_nc_cat=108&ccb=1-7&_nc_sid=aee45a&_nc_ohc=-SDiNVUe94wAX-iQMna&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVI-4E4yCkoYuEwSApM8CRGtic_zpfr6_YhZsGcvx2x-_g&oe=631B372F"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
